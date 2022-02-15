e1d32918e80e187fb745adaed4e6f7811b0058c5
dohuyhoa <dohuyhoa.jaekin@gmail.com>
2022-02-16 01:38:34 +0700

    Lesson07

---
+++ example/Script/main.js
@@ -0,0 +1,21 @@
+// alert('Hieu Thu Hai');
+// var fullName = 'Đỗ Huy Hòa';
+// L
+// var age = 26;
+// alert(fullName);
+// alert(age);
+// var x = 5;
+// var y = 3
+// var z = 5;
+// var t;
+/* document.write('<br/>x = ' + x + '<br/>y = ' + y)
+document.write(`<br/>x = ${x}<br/>y = ${y}`)
+t = x++ + ++x - y-- + z++ + 2;
+console.log(t);
+console.log(`t= ${t}`);
+document.write(`<br/>x = ${x}, y = ${y}, z = ${z}, t = ${t}`) */
+var x;
+var mycars = ['Saab', 'volvo', 'BMW'];
+for (x in mycars) {
+    document.write(mycars[x]);
+}
